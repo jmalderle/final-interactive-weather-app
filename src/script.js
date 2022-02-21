a283f018ffe957d0e05c286a1276f4e7c0078701
jmalderle <96013438+jmalderle@users.noreply.github.com>
2022-02-21 20:42:06 +0100

    removed cet to see if time changes

--- src/script.js
+++ src/script.js
@@ -20,7 +20,7 @@ function formatDate(timestamp) {
   ];
 
   let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes} CET`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayForecast() {
