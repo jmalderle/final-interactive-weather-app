48f6d51b0809f185defb2b29852f381e661ac8cb
jmalderle <96013438+jmalderle@users.noreply.github.com>
2022-02-27 19:51:36 +0100

    adding sunset and sunrise

--- src/script.js
+++ src/script.js
@@ -31,6 +31,19 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+function formatSuntimeDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes} CET`;
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -87,6 +100,8 @@ function displayTemperature(response) {
   let countryElement = document.querySelector("#country");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let sunriseElement = document.querySelector("#sunrise");
+  let sunsetElement = document.querySelector("#sunset");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
@@ -98,6 +113,10 @@ function displayTemperature(response) {
   countryElement.innerHTML = response.data.sys.country;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  sunriseElement.innerHTML = formatSuntimeDate(
+    response.data.sys.sunrise * 1000
+  );
+  sunsetElement.innerHTML = formatSuntimeDate(response.data.sys.sunset * 1000);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
@@ -118,6 +137,9 @@ function getWeatherGif(response) {
   } else if (weatherType === "Rain") {
     weatherTypeElement.innerHTML = `
     <img src="media/rain.gif" class="weatherTypeGif" alt="rain" />`;
+  } else if (weatherType === "Drizzle") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/drizzle.gif" class="weatherTypeGif" alt="drizzle" />`;
   } else if (weatherType === "Thunderstorm") {
     weatherTypeElement.innerHTML = `
     <img src="media/thunderstorm.gif" class="weatherTypeGif" alt="thunderstorm" />`;
@@ -127,6 +149,43 @@ function getWeatherGif(response) {
   } else if (weatherType === "Mist") {
     weatherTypeElement.innerHTML = `
     <img src="media/mist.gif" class="weatherTypeGif" alt="mist"  />`;
+  } else if (weatherType === "Fog") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/fog.gif" class="weatherTypeGif" alt="fog"  />`;
+  } else if (weatherType === "smoke") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/smoke.gif" class="weatherTypeGif" alt="smoke"  />`;
+  } else if (weatherType === "Haze") {
+    weatherTypeElement.innerHTML = null;
+  } else if (weatherType === "Dust") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/dust.gif" class="weatherTypeGif" alt="dust"  />`;
+  } else if (weatherType === "Sand") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/sand.gif" class="weatherTypeGif" alt="sand"  />`;
+  } else if (weatherType === "Ash") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/ash.gif" class="weatherTypeGif" alt="ash"  />`;
+  } else if (weatherType === "Squall") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/squall.gif" class="weatherTypeGif" alt="squall"  />`;
+  } else if (weatherType === "Tornado") {
+    weatherTypeElement.innerHTML = `
+    <img src="media/tornado.gif" class="weatherTypeGif" alt="tornado"  />`;
+  } else if (weatherType === "Clouds") {
+    weatherTypeElement.innerHTML = `<img src="media/clouds.gif" class="weatherTypeGif" alt="clouds"  />`;
+  }
+}
+
+function showColoredCityNames(response) {
+  let specialCity = response.data.sys.name;
+  let specialCityElement = document.querySelector("#city");
+  if (specialCity === "Paris") {
+    specialCityElement.innerHTML = `
+    RED`;
+  } else if (specialCity === "Paris") {
+    specialCityElement.innerHTML = `
+    RED`;
   }
 }
 
@@ -135,6 +194,7 @@ function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
   axios.get(apiUrl).then(getWeatherGif);
+  axios.get(apiUrl).then(showColoredCityNames);
 }
 
 function handleSubmit(event) {
